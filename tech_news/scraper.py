d27d6615065fe0933a31f1422833edca8541c170
GiovannaMorais <gigimorais9@hotmail.com>
2023-02-08 16:06:49 -0300

    Feat: Create the scrape_news function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -36,7 +36,25 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    url = selector.css('link[rel="canonical"]::attr(href)').get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("span.author > a::text").get()
+    reading_time = selector.css("li.meta-reading-time::text").get().split()[0]
+    summary = "".join(
+      selector.css(".entry-content > p:nth-of-type(1) *::text").getall()
+      ).strip()
+    category = selector.css("a > span.label::text").get()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": int(reading_time),
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
