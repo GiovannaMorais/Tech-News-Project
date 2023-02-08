7b295437275a7973dad114a9d3057b92f27fcbf9
GiovannaMorais <gigimorais9@hotmail.com>
2023-02-08 17:23:49 -0300

    Feat: Create the search_by_title function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    searched_news = []
+    query = {"title": {"$regex": title, "$options": "i"}}
+    for new in search_news(query):
+        searched_news.append((new["title"], new["url"]))
+    return searched_news
 
 
 # Requisito 8
