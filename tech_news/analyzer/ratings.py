16ced404049e0d74f445ff2dd3427622d6dbc4a8
GiovannaMorais <gigimorais9@hotmail.com>
2023-02-08 18:23:21 -0300

    Feat: Create the top_5_categories function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,21 @@
 # Requisito 10
+from tech_news.database import find_news
+from collections import Counter
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = []
+    for new in find_news():
+        categories.append(new["category"])
+
+    categories = [
+        {"name": name, "quantity": quantity}
+        for name, quantity in Counter(categories).items()
+    ]
+    # print("categories", categories)
+    sort = sorted(
+        categories,
+        key=lambda category: (-category["quantity"], category["name"]))
+
+    categories_sorted_result = [category["name"] for category in sort]
+    return categories_sorted_result
